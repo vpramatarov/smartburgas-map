91c4b74c2d9e05fa0872e7af1c2c1c1079d7a2ea
vpramatarov <velizarpamatarov@gmail.com>
2026-01-10 15:04:30 +0200

    server.ts: switch to ES Module system

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import path from 'path';
 import fs from 'fs/promises';
 import {existsSync, mkdirSync} from 'fs';
 
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 const airQualityTarget: Target = {
